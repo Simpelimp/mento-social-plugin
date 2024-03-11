7cf7c509e552ada5f6a3a0c6626c0e47ae191935
simpelimp <zack-2@hotmail.com>
2024-03-11 19:01:11 +0100

    wip

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -8,4 +8,4 @@ const devSupabaseUrl = "https://ckpzvsojbtghkcowikmq.supabase.co";
 const devAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrcHp2c29qYnRnaGtjb3dpa21xIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE3OTU1MDYsImV4cCI6MTk5NzM3MTUwNn0.3Vh9i_Mkz9CbKG8FKmORrAxptHquXxE4WqHe8lL77tI";
 
-export const supabase = createClient(devSupabaseUrl, devAnonKey);
+export const supabase = createClient(supabaseUrl, anonKey);
